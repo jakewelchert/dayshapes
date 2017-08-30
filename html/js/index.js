e69e793ccf34af6e2547671bab7575fba70cbc2f
Jake Welchert <Ambitiative@Jakes-MacBook-Pro.local>
2017-08-29 23:09:05 -0500

    Init

--- html/js/index.js
+++ html/js/index.js
@@ -1,20 +1,7 @@
-$("a").click(function(){
-  $(this).toggleClass("open");
-});
-
-
-window.onload = function() {
-  var slideout = new Slideout({
-    'panel': document.getElementById('panel'),
-    'menu': document.getElementById('menu'),
-    'side': 'right'
+(function($) {
+  $(function() {
+    $('.toggle-overlay').click(function() {
+      $('aside').toggleClass('open');
+    });
   });
-
-  document.querySelector('.js-slideout-toggle').addEventListener('click', function() {
-    slideout.toggle();
-  });
-
-  document.querySelector('.menu').addEventListener('click', function(eve) {
-    if (eve.target.nodeName === 'A') { slideout.close(); }
-  });
-};
+})(jQuery);
